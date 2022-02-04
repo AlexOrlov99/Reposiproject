407e3f45ecffa4695ded86c55d521740dfc9b0c2
AlexOrlov99 <orlov_aleksey_95@inbox.ru>
2022-02-04 20:14:55 +0500

    v0.2

--- apps/firstapp/admin.py
+++ apps/firstapp/admin.py
@@ -1,8 +1,10 @@
 from typing import Optional
 from django.contrib import admin
 from django.core.handlers.wsgi import WSGIRequest
-# Register your models here.
 from . models import Account
+from . models import Group
+from . models import Student
+
 
 class AccountAdmin(admin.ModelAdmin):
     readonly_fields = ()
@@ -15,6 +17,43 @@ class AccountAdmin(admin.ModelAdmin):
         if obj:
             return self.readonly_fields + ('description',)
         return self.readonly_fields
+
 admin.site.register(
     Account,AccountAdmin
 )
+
+
+class GroupAdmin(admin.ModelAdmin):
+    readonly_fields = ()
+
+    def get_readonly_fields(
+        self,
+        request: WSGIRequest,
+        obj: Optional[Account] = None
+    ) -> tuple:
+        if obj:
+            return self.readonly_fields + ('name',)
+        return self.readonly_fields
+
+
+admin.site.register(
+    Group, GroupAdmin
+    )
+
+
+class StudentAdmin(admin.ModelAdmin):
+    readonly_fields = ()
+    STUDENT_MAX_AGE = 16
+
+    def get_readonly_fields(
+        self,
+        request: WSGIRequest,
+        obj: Optional[Account] = None
+    ) -> tuple:
+        if obj and obj.age > STUDENT_MAX_AGE:
+            return self.readonly_fields + ('age',)
+        return self.readonly_fields
+
+admin.site.register(
+    Student, StudentAdmin
+    )
--- apps/firstapp/models.py
+++ apps/firstapp/models.py
@@ -16,7 +16,7 @@ class Account(models.Model):
     description = models.TextField()
 
     def __str__(self) -> str:
-        return f'Account: {self.user.id} / {self.full_name}' 
+        return f'Account: {self.user.id}  {self.full_name}' 
 
     class Meta:
         ordering = (
@@ -24,3 +24,47 @@ class Account(models.Model):
         )
         verbose_name = 'Аккаунт'
         verbose_name_plural = 'Аккаунты'
+
+class Group(models.Model):
+    GROUP_NAME_MAX_LENGTH = 10
+
+    name = models.CharField(
+        max_length = GROUP_NAME_MAX_LENGTH
+    )
+    def __str__(self) -> str:
+        return f'Group: {self.name}'
+
+    class Meta:
+        ordering = (
+            'name',
+        )
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+class Student(models.Model):
+
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE
+    )
+    age = models.IntegerField(
+        'Возраст студента'
+    )
+    gpa = models.FloatField(
+        'Средний бал'
+    )
+    group = models.ForeignKey(
+        Group, on_delete=models.PROTECT
+    )
+    def __str__(self) -> str:
+        return f'Student: {self.account}, {self.age}, \
+            {self.gpa}, {self.group.name}'
+    
+    class Meta:
+        ordering = (
+            'account',
+            'age',
+            'group',
+            'gpa'
+        )
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Стундеты'
